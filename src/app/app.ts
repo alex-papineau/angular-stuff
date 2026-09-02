f567c01b03c4439097e7f1f333cae80b61727345
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-01 20:16:46 -0500

    replace RouterOutlet with Stopwatch component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Stopwatch } from './stopwatch/stopwatch';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Stopwatch],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('Angular-Portfolio');
 }

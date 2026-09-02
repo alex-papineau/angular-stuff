c90f6de425283cf9968d4955d6ac03642ac445c5
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-01 21:47:00 -0500

    add PasswordGenerator component to app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Stopwatch } from './stopwatch/stopwatch';
 import { Quiz } from './quiz/quiz';
+import { PasswordGenerator } from './password-generator/password-generator';
 
 @Component({
   selector: 'app-root',
-  imports: [Stopwatch, Quiz],
+  imports: [Stopwatch, Quiz, PasswordGenerator],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

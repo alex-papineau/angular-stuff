07db2945471988773e460fbe9cf69cb00a7259b7
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-01 21:35:24 -0500

    update tests to reflect new app title and add e2e configuration

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,5 +4,13 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/MyApp/);
+  await expect(page).toHaveTitle(/Angular Stuff/);
 });
+
+test('has main heading', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect heading to contain Angular Stuff
+  await expect(page.getByRole('heading', { name: 'Angular Stuff', level: 1 })).toBeVisible();
+});
+

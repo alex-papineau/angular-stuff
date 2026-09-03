a543193ac6e42ca34b612a4ebc632a52933ac1bc
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-03 13:22:50 -0500

    feat: implement password generation functionality and update component structure

--- src/app/password-generator/password-generator.ts
+++ src/app/password-generator/password-generator.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-password-generator',
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './password-generator.html',
   styleUrl: './password-generator.css',
 })
-export class PasswordGenerator {}
+export class PasswordGenerator {
+
+  password = signal('');
+  generatePassword() {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    
+    let pass = '';
+    const randomValues = new Uint32Array(12);
+    crypto.getRandomValues(randomValues);
+
+    for (let i = 0; i < 12; i++) {
+      pass += chars[randomValues[i] % chars.length];
+    }
+    this.password.set(pass);
+  }
+}

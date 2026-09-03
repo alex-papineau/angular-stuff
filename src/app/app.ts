bffc4bf822f9c90e6fd9821c04e1665b550c50cb
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-03 13:25:02 -0500

    feat: add chat UI and GitHub user search components to app imports and template

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Stopwatch } from './stopwatch/stopwatch';
 import { Quiz } from './quiz/quiz';
 import { PasswordGenerator } from './password-generator/password-generator';
+import { ChatUI } from './chat-ui/chat-ui';
+import { GithubUserSearchUI } from './github-user-search-ui/github-user-search-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [Stopwatch, Quiz, PasswordGenerator],
+  imports: [Stopwatch, Quiz, PasswordGenerator, ChatUI, GithubUserSearchUI],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

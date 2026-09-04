03d6893a3a0f8e783acfb4edb7d025d8eae404af
@ <99501899+alex-papineau@users.noreply.github.com>
2026-09-04 01:42:18 -0500

    feat: add Digital World Clock component and update app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,10 +9,11 @@ import { VotingApp } from './voting-app/voting-app';
 import { NotesApp } from './notes-app/notes-app';
 import { MiniCartSystem } from './mini-cart-system/mini-cart-system';
 import { TrafficSignal } from './traffic-signal/traffic-signal';
+import { DigitalWorldClock } from './digital-world-clock/digital-world-clock';
 
 @Component({
   selector: 'app-root',
-  imports: [Stopwatch, Quiz, PasswordGenerator, ChatUI, GithubUserSearchUI, VotingApp, NotesApp, MiniCartSystem, TrafficSignal],
+  imports: [Stopwatch, Quiz, PasswordGenerator, ChatUI, GithubUserSearchUI, VotingApp, NotesApp, MiniCartSystem, TrafficSignal, DigitalWorldClock],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
